19fbc410e58f18bbda1880337c5107019308d5c9
David Morales <davidmorales9611@gmail.com>
2022-03-12 18:49:10 -0500

    add animation to pokemon appbar

--- lib/src/widgets/pokemon_app_bar.dart
+++ lib/src/widgets/pokemon_app_bar.dart
@@ -5,42 +5,78 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../bloc/pokemon_details/pokemon_details_cubit.dart';
 import '../model/main.dart';
 
-class PokemonAppBar extends StatelessWidget {
+class PokemonAppBar extends StatefulWidget {
   const PokemonAppBar({Key? key, required this.pokemonPaginationResult})
       : super(key: key);
   final PokemonPaginationResult pokemonPaginationResult;
 
   @override
+  State<PokemonAppBar> createState() => _PokemonAppBarState();
+}
+
+class _PokemonAppBarState extends State<PokemonAppBar>
+    with SingleTickerProviderStateMixin {
+  late final _controller = AnimationController(
+    duration: const Duration(milliseconds: 500),
+    vsync: this,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<PokemonDetailsCubit, PokemonDetailsState>(
+    final theme = Theme.of(context);
+    return BlocConsumer<PokemonDetailsCubit, PokemonDetailsState>(
+      listener: (context, state) {
+        if (state is PokemonDetailsLoaded) {
+          _controller.forward();
+        }
+      },
       builder: (context, state) {
         final color = (state is PokemonDetailsLoaded)
             ? typeColors[state.pokemon.types!.first.type!.name]
             : null;
-        return SliverAppBar(
-          backgroundColor: color,
-          floating: true,
-          pinned: true,
-          expandedHeight: 400.0,
-          flexibleSpace: FlexibleSpaceBar(
+        final animation = ColorTween(
+          begin: theme.appBarTheme.backgroundColor,
+          end: color,
+        ).animate(_controller);
+        return AnimatedBuilder(
+          animation: animation,
+          builder: (BuildContext context, Widget? child) {
+            return SliverAppBar(
+              backgroundColor: _controller.isCompleted ? color : null,
+              floating: true,
+              pinned: true,
+              expandedHeight: 400.0,
+              flexibleSpace: child,
+            );
+          },
+          child: FlexibleSpaceBar(
             centerTitle: true,
             title: Text(
-              "${pokemonPaginationResult.name[0].toUpperCase()}${pokemonPaginationResult.name.substring(1)}",
-              style: Theme.of(context)
-                  .appBarTheme
-                  .titleTextStyle!
-                  .copyWith(height: .8),
+              "${widget.pokemonPaginationResult.name[0].toUpperCase()}${widget.pokemonPaginationResult.name.substring(1)}",
+              style: theme.appBarTheme.titleTextStyle!.copyWith(height: .8),
               overflow: TextOverflow.ellipsis,
             ),
-            background: Container(
-              color: color,
+            background: AnimatedBuilder(
+              animation: animation,
+              builder: (BuildContext context, Widget? child) {
+                return Container(
+                  color: animation.value,
+                  child: child!,
+                );
+              },
               child: Center(
                 child: Hero(
-                  tag: pokemonPaginationResult.number,
+                  tag: widget.pokemonPaginationResult.number,
                   child: Transform.scale(
                     scale: 3.5,
                     child: CachedNetworkImage(
-                      imageUrl: pokemonPaginationResult.imageUrl,
+                      imageUrl: widget.pokemonPaginationResult.imageUrl,
                       placeholder: (context, url) =>
                           const CircularProgressIndicator(),
                       errorWidget: (context, url, error) =>
