af3f15cde581c94230ab315e6d49f7c421ce8219
David Morales <davidmorales9611@gmail.com>
2022-03-13 16:37:16 -0500

    add mouse hover to card

--- lib/src/widgets/pokemon_card.dart
+++ lib/src/widgets/pokemon_card.dart
@@ -14,58 +14,61 @@ class PokemonCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) {
-              return PokemonDetailsScreen(
-                pokemonPaginationResult: pokemonPaginationResult,
-              );
-            },
-          ),
-        );
-      },
-      child: Card(
-        elevation: 2,
-        child: Column(
-          children: [
-            Expanded(
-              child: Hero(
-                tag: pokemonPaginationResult.number,
-                child: Transform.scale(
-                  scale: 1.2,
-                  child: CachedNetworkImage(
-                    imageUrl: pokemonPaginationResult.imageUrl,
-                    errorWidget: (context, url, error) => const Image(
-                      image: AssetImage('assets/failed_pokemon.png'),
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return PokemonDetailsScreen(
+                  pokemonPaginationResult: pokemonPaginationResult,
+                );
+              },
+            ),
+          );
+        },
+        child: Card(
+          elevation: 2,
+          child: Column(
+            children: [
+              Expanded(
+                child: Hero(
+                  tag: pokemonPaginationResult.number,
+                  child: Transform.scale(
+                    scale: 1.2,
+                    child: CachedNetworkImage(
+                      imageUrl: pokemonPaginationResult.imageUrl,
+                      errorWidget: (context, url, error) => const Image(
+                        image: AssetImage('assets/failed_pokemon.png'),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: Text(
-                    "No. ${pokemonPaginationResult.number}",
-                    style: Theme.of(context).textTheme.bodyText2!.copyWith(
-                          height: 0,
-                          color: Theme.of(context).textTheme.caption?.color,
-                        ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      "No. ${pokemonPaginationResult.number}",
+                      style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                            height: 0,
+                            color: Theme.of(context).textTheme.caption?.color,
+                          ),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Text(
+                    "${toBeginningOfSentenceCase(pokemonPaginationResult.name)}",
+                    style: Theme.of(context).textTheme.headline6,
                     overflow: TextOverflow.ellipsis,
                   ),
-                ),
-                Text(
-                  "${toBeginningOfSentenceCase(pokemonPaginationResult.name)}",
-                  style: Theme.of(context).textTheme.headline6,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
